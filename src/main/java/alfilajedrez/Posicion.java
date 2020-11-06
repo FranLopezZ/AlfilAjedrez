e3ee1af4e3b5fa5d2969d5c6cb9366fe65893b46
Fran Lopez <43579925+FranLopezZ@users.noreply.github.com>
2020-11-06 19:12:28 +0100

    7.Creacion de constructor con validacion mediante SET.

--- src/main/java/alfilajedrez/Posicion.java
+++ src/main/java/alfilajedrez/Posicion.java
@@ -4,9 +4,17 @@ public class Posicion {
 	private int fila;
 	private char columna;
 	
+	
+	
+	public Posicion(int fila, char columna) {
+		setFila(fila);
+		setColumna(columna);
+	}
+	
 	public int getFila() {
 		return fila;
 	}
+	
 	private void setFila(int fila) {
 		if (fila < 1 || fila > 8) {
 			throw new IllegalArgumentException("La fila es incorrecta. Las filas van desde la 1 a la 8.");
@@ -14,9 +22,11 @@ public class Posicion {
 			this.fila = fila;
 		}
 	}
+	
 	public char getColumna() {
 		return columna;
 	}
+	
 	private void setColumna(char columna) {
 		if (columna < 'a' || columna > 'h') {
 			throw new IllegalArgumentException("La columna es incorrecta. Las columnas van desde la 'a' a la 'h'.");
@@ -25,5 +35,6 @@ public class Posicion {
 		}
 	}
 }
+
 
 
