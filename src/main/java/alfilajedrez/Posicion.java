59b9695edd51ae9966b07b93626cfa17157afc1d
Fran Lopez <43579925+FranLopezZ@users.noreply.github.com>
2020-11-06 19:00:51 +0100

    6.Creacion de metodos GET & SET con validacion y mensaje de error.

--- src/main/java/alfilajedrez/Posicion.java
+++ src/main/java/alfilajedrez/Posicion.java
@@ -3,4 +3,27 @@ package alfilajedrez;
 public class Posicion {
 	private int fila;
 	private char columna;
+	
+	public int getFila() {
+		return fila;
+	}
+	private void setFila(int fila) {
+		if (fila < 1 || fila > 8) {
+			throw new IllegalArgumentException("La fila es incorrecta. Las filas van desde la 1 a la 8.");
+		} else {
+			this.fila = fila;
+		}
+	}
+	public char getColumna() {
+		return columna;
+	}
+	private void setColumna(char columna) {
+		if (columna < 'a' || columna > 'h') {
+			throw new IllegalArgumentException("La columna es incorrecta. Las columnas van desde la 'a' a la 'h'.");
+		} else {
+			this.columna = columna;
+		}
+	}
 }
+
+
